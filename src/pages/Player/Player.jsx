a3e87dba8286ea9e8e50bd158ce5a7aaafcd83f4
Le Duy Vu <leduyvupro123@gmail.com>
2026-03-13 19:12:42 +0700

    update trailer

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Player = () => {
-  return <div className="player"></div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWRlOWJjZmE4ODRiNDUzODNkZTA1ZDZiY2I1ZTE4NSIsIm5iZiI6MTc3MzM5ODgzNy4zNDMwMDAyLCJzdWIiOiI2OWIzZWIzNWYxMGIyMjQwMzVkZjRjZjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.bdq3kAsBkMdQY2NG8DsaizYO-BjU8HolbHxMio8BJ1A",
+    },
+  };
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos`, options)
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        frameBorder="0"
+        allowFullscreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
