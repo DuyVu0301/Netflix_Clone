67accf32db12c3547a5284e4c324e883915d5cbe
Le Duy Vu <leduyvupro123@gmail.com>
2026-03-13 18:20:26 +0700

    call api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,25 +1,46 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({ title, category }) => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWRlOWJjZmE4ODRiNDUzODNkZTA1ZDZiY2I1ZTE4NSIsIm5iZiI6MTc3MzM5ODgzNy4zNDMwMDAyLCJzdWIiOiI2OWIzZWIzNWYxMGIyMjQwMzVkZjRjZjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.bdq3kAsBkMdQY2NG8DsaizYO-BjU8HolbHxMio8BJ1A",
+    },
+  };
+
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
   const handleWheel = (e) => {
     e.preventDefault();
     cardsRef.current.scrollLeft += e.deltaY;
   };
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
   return (
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
